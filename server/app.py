3f43ae76219dd816701ef58aa76eb2f5e21b2af6
shallon <shallonmokeira@gmail.com>
2023-10-30 21:43:57 +0300

    updates

---
+++ server/app.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, request, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Plant
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+
+class Plants(Resource):
+
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
+
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
+
+
+api.add_resource(Plants, '/plants')
+
+
+class PlantByID(Resource):
+
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+        record = Plant.query.filter_by(id=id).first()
+        for attr, value in data.items():
+            setattr(record, attr, value)
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response
+    
+    def delete(self, id):
+
+        record = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response = make_response("", 204)
+
+        return response
+
+
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)

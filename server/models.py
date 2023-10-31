686f3bb76920465b62c96bd064e477a438ade782
shallon <shallonmokeira@gmail.com>
2023-10-31 18:14:30 +0300

    add models

--- server/models.py
+++ server/models.py
@@ -9,8 +9,32 @@ class Movie(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    release_date = db.Column(db.Date)
+    description = db.Column(db.Text)
+    
+class Genre(db.Model, SerializerMixin):
+    __tablename__ = 'genres'
 
-    def __repr__(self):
-        return f'<Movie {self.name} | In Stock: {self.is_in_stock}>'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+class Rating(db.Model, SerializerMixin):
+    __tablename__ = 'ratings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)    
+
+    

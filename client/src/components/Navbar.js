3e55b0538ff428859666211fa7107ea041bd4746
shallon <shallonmokeira@gmail.com>
2023-11-03 09:10:18 +0300

    add navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; // Import the CSS file
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/movies">Movies</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/addMovie">Add Movie</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/userProfile">User Profile</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
